2034f89d1897298c461682dad43c6e54156de9a6
Zeeshan <zeeshan.mohammad@hotmail.com>
2022-09-19 21:04:44 +0530

    weather app code complete

---
+++ weatherapp.ui/weatherapp-ui/src/app/models/weather.model.ts
@@ -0,0 +1,8 @@
+export interface WeatherData {
+    cityName: string
+    temperature: number
+    minimumTemperature: number
+    maximumTemperature: number
+    humidity: number
+    windSpeed: number
+}
